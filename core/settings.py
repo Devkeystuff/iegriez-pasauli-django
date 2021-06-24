21bc3f846e289670fb04fdb0f27addbfd5a64a48
Devkeystuff <ronalds.palacis@outlook.com>
2021-06-25 01:20:40 +0300

    Added staticfiles_dirs

--- core/settings.py
+++ core/settings.py
@@ -156,6 +156,11 @@ LOGGING = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
