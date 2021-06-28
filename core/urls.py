6597266eedb2963e34e08b57b5c0fdeedf4c6bfc
Devkeystuff <ronalds.palacis@outlook.com>
2021-06-28 14:08:37 +0300

    Added urls back

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    # path('', include('game.urls')),
-    # path('', include('users.urls')),
+    path('', include('game.urls')),
+    path('', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
 
