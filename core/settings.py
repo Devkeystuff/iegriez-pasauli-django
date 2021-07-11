ad2322db09b0a8dd46b19dfa0ca06c6460b283cc
Devkeystuff <ronalds.palacis@outlook.com>
2021-07-11 23:04:11 +0300

    Allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-2%jg#bxcd(vute4m%@hxr3a(k^u-cpaxcb$8_5*c)&59&27+ad
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['iegriez-pasauli.herokuapp.com',
+ALLOWED_HOSTS = ['https://iegriez-pasauli.herokuapp.com',
                  'https://iegriez-pasauli-backend.herokuapp.com', 'localhost']
 
 CORS_ALLOWED_ORIGINS = [
